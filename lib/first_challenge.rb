86a07adf55fc4798fb2ac5d887491ec088de7466
Sherzod Karimov <karimovj@gmail.com>
2018-07-23 16:17:50 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,10 +13,9 @@ def first_challenge
     }
   }
 
-  #your code here
-
-
-  #remember to return your newly altered contacts hash!
+  
+  contacts["Freddy Mercury"][:favorite_icecream_flavors].delete("strawberry")
+  
   contacts
 end
 
